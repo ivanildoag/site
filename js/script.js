cb41229635a20acc9a158aa30f00a985a8ebdb38
Ivanildo Almeida <ivanildoag@gmail.com>
2024-09-02 19:21:09 -0300

    Atualização CSS varios estilos

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 
 /* ====Truns the menu hambutguer intoa X === */
 
-const menuHamburguer= document.querySelector('.meu-hamburguer')
-     menuHamburguer.addEventListener('click', () => {
-        toggleMenu();
+const menuHamburguer = document.querySelector('.meu-hamburguer')
+const nav = document.querySelector('.nav-responsive');
+
+menuHamburguer.addEventListener('click', () => {
+    toggleMenu();
 });
 
 function toggleMenu() {
-    const nav = document.querySelector( '.nav-responsive');
-    menuHamburguer.classList.toggle( 'change');
+    menuHamburguer.classList.toggle('change');
 
-    if ( menuHamburguer.classList.caontains('change')) {
+    if (menuHamburguer.classList.caontains('change')) {
         nav.computedStyleMap.display = 'block';
 
     } else {
