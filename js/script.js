8007be6928d486c3356ad04e37976473caedfc4f
Ivanildo Almeida <ivanildoag@gmail.com>
2024-09-02 18:27:11 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 
-/* alert("hellow");*/
+/* ====Truns the menu hambutguer intoa X === */
+
+const menuHamburguer= document.querySelector('.meu-hamburguer')
+     menuHamburguer.addEventListener('click', () => {
+        toggleMenu();
+});
+
+function toggleMenu() {
+    const nav = document.querySelector( '.nav-responsive');
+    menuHamburguer.classList.toggle( 'change');
+
+    if ( menuHamburguer.classList.caontains('change')) {
+        nav.computedStyleMap.display = 'block';
+
+    } else {
+        nav.computedStyleMap.dispaly = 'none';
+    }
+
+}
